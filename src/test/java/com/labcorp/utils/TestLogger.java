d105408b88b8ab6ee43f8188925e63ce9b7e735c
codemonk-in <mishraapurv@yahoo.co.in>
2025-07-07 17:13:37 +0530

    added timestamp to log file name

--- src/test/java/com/labcorp/utils/TestLogger.java
+++ src/test/java/com/labcorp/utils/TestLogger.java
@@ -8,11 +8,19 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * Utility class for centralized logging of test execution status, messages, and scenarios.
+ * Creates a new timestamped log file on each test execution.
  */
 public class TestLogger {
 
-    private static final String LOG_FILE_PATH = "target/test-execution-log.txt";
     private static final DateTimeFormatter TIMESTAMP_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter FILE_NAME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+    private static final String LOG_FILE_PATH;
+
+    static {
+        // Generate log file name based on current timestamp
+        String timestamp = FILE_NAME_FORMAT.format(LocalDateTime.now());
+        LOG_FILE_PATH = "target/test-execution-log-" + timestamp + ".txt";
+    }
 
     /**
      * Logs a generic message with timestamp to both console and file.
@@ -44,7 +52,7 @@ public class TestLogger {
     }
 
     /**
-     * Appends a line to the log file.
+     * Appends a line to the timestamped log file.
      * @param content The content to append.
      */
     private static void appendToFile(String content) {
